a0225f5086c99fbe750bc632a13151f7b391a454
Venkatesh Ch <venk120soft@gmail.com>
2021-02-22 20:52:12 -0500

    Update mock-objects.ts

--- mock-objects.ts
+++ mock-objects.ts
@@ -1,13 +1,44 @@
 import { EventSchema, telemetryService } from '.';
 import { telemetryHelper } from './telemetry-helper';
+import { shallow } from 'enzyme';
 
 jest.mock('common/config');
 jest.mock('common/config/history');
 jest.mock('./telemetry.service');
+jest.mock('react-dom', () => ({
+  findDOMNode: jest.fn()
+}));
 
 describe('Event tracker for telemetry helper', () => {
+   const testButton: any = <button>TestButton</button>;
+  const mockContext: string = 'Mock Context';
+  let component: any;
+  let instance: any;
+  const trackUserEventSpy = jest.spyOn(telemetryService, 'trackUserEvent');
+  
+  beforeEach(() => {
+    component = shallow(
+      <TelemetryTracker feature={'Test'} subFeature={'Test Sub'}
+        action={`TestButtonClicked`} context={mockContext}>
+        {testButton}
+      </TelemetryTracker>
+    );
+    instance = component.instance();
+  });
+
+  it('should render Telemetry Tracker properly', () => {
+    expect(component).toMatchSnapshot();
+  });
+  it('should handle events of its children', () => {
+    const buttonComponent = component;
+    (component.instance() as any).handleEvent = jest.fn(); 
+    component.find('button').simulate('click');
+    // Need to set timeout here to sense the change apparently
+    setTimeout(() => {
+      expect((component.instance() as any).handleEvent).toHaveBeenCalled();
+    }, 0);
+  });
   it('should be able to handle events ', () => {
-    const trackUserEventSpy = jest.spyOn(telemetryService, 'trackUserEvent');
     const mockObj = {
       feature: 'Test',
       subFeature: 'Test sub',
